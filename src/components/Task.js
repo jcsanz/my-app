66ba8b7fdc09a3fc856a535eb73772449736c2d3
unknown <sanz.carl.gt@gmail.com>
2021-08-13 09:10:06 -0500

    Final

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,13 @@
 const Task = (props) =>{
   return(
-    <div className='task'>
-      <h3>{props.text}  <button className='delete'>X
+    <div className={`task  ${props.reminder ? 'reminder' : ''}`} onDoubleClick={() => props.onToggle(props.id)}>
+      <h3>{props.text}  <button className='delete' onClick={() => props.onDelete(props.id)}>X
       </button></h3>
       <p>{props.day}</p>
     </div>
+  
   )
   }
   export default Task
+  
   
